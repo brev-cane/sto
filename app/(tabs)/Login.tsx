ca1149806ba141b6669cad5b6a32eab0464bf1aa
cam6573 <cam6573@g.rit.edu>
2025-08-11 16:42:52 -0400

    made login screen pretty and made apple button prompt a sign in using apple, does not currently prompt a screen change to the main app after login.

--- app/(tabs)/Login.tsx
+++ app/(tabs)/Login.tsx
@@ -3,21 +3,68 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import React, { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import {
   ActivityIndicator,
-  Button,
   KeyboardAvoidingView,
   StyleSheet,
   TextInput,
   View,
+  TouchableOpacity,
+  Text,
+  Platform,
+  ScrollView,
 } from "react-native";
 import { FIREBASE_AUTH, FIRESTORE_DB } from "../../FirebaseConfig";
 import { doc, setDoc } from "firebase/firestore";
 import { registerForPushNotificationsAsync } from "../components/notifications";
 import { useAlert } from "@/contexts/dropdownContext";
 import { useNavigation } from "@react-navigation/native";
+import { AppleButton, appleAuth } from '@invertase/react-native-apple-authentication';
+import { AppleAuthProvider, getAuth, revokeAccessToken, onAuthStateChanged, signInWithCredential, } from '@react-native-firebase/auth';
+
 
 const Login = () => {
+
+  // async function revokeSignInWithAppleToken() {
+  //   // Get an authorizationCode from Apple
+  //   const { authorizationCode } = await appleAuth.performRequest({
+  //     requestedOperation: appleAuth.Operation.REFRESH,
+  //   });
+  
+  //   // Ensure Apple returned an authorizationCode
+  //   if (!authorizationCode) {
+  //     throw new Error('Apple Revocation failed - no authorizationCode returned');
+  //   }
+  
+  //   // Revoke the token
+  //   return revokeAccessToken(getAuth(), authorizationCode);
+  // }
+
+  async function onAppleButtonPress() {
+    // Start the sign-in request
+    const appleAuthRequestResponse = await appleAuth.performRequest({
+      requestedOperation: appleAuth.Operation.LOGIN,
+      // As per the FAQ of react-native-apple-authentication, the name should come first in the following array.
+      // See: https://github.com/invertase/react-native-apple-authentication#faqs
+      requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
+    });
+  
+    // Ensure Apple returned a user identityToken
+    if (!appleAuthRequestResponse.identityToken) {
+      throw new Error('Apple Sign-In failed - no identify token returned');
+    }
+  
+    // Create a Firebase credential from the response
+    //console.log(appleAuthRequestResponse.identityToken);
+    const { identityToken, nonce } = appleAuthRequestResponse;
+    const appleCredential = AppleAuthProvider.credential(identityToken, nonce);
+ 
+  
+    // Sign the user in with the credential
+    const response = await  signInWithCredential(getAuth(), appleCredential);
+  }
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
@@ -119,12 +166,18 @@ const Login = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <KeyboardAvoidingView behavior="padding">
+    <ScrollView contentContainerStyle={styles.container}>
+    <View style={styles.scroll}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        style={styles.innerContainer}
+      >
+        <Text style={styles.title}>Sign in</Text>
         <TextInput
           value={email}
           style={styles.input}
           placeholder="Email"
+          placeholderTextColor="#999"
           autoCapitalize="none"
           onChangeText={(text) => setEmail(text)}
         ></TextInput>
@@ -133,36 +186,141 @@ const Login = () => {
           value={password}
           style={styles.input}
           placeholder="password"
+          placeholderTextColor="#999"
           autoCapitalize="none"
           onChangeText={(text) => setPassword(text)}
         ></TextInput>
 
-        {loading ? (
-          <ActivityIndicator size="large" color="#0000ff" />
+      {loading ? (
+          <ActivityIndicator size="large" color="#007AFF" style={styles.loader} />
         ) : (
           <>
-            <Button title="Login" onPress={() => signIn()} />
-            <Button title="Create account" onPress={() => navigate("Signup")} />
+            <TouchableOpacity style={styles.button} onPress={signIn}>
+              <Text style={styles.buttonText}>Log in</Text>
+            </TouchableOpacity>
+            <Text style={styles.subtitleText}> - OR - </Text>
+
+            <TouchableOpacity
+              style={[styles.button, styles.googleButton]}
+              onPress={() => navigate("Login")}
+            >
+              <Ionicons name="logo-google" size={24} color="#007AFF" />
+              <Text style={styles.googleButtonText}> Sign in with Google</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.googleButton}
+              onPress={() => onAppleButtonPress().then(() => console.log('Apple sign-in complete!'))}
+            >
+              <Ionicons name="logo-apple" size={24} color="#007AFF" />
+              <Text style={styles.googleButtonText}> Sign in with Apple</Text>
+            </TouchableOpacity>
+
+            <View style={{
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: "center",
+            }}>
+              <Text style={styles.subtitleText}>Don't have an account? </Text>
+              <TouchableOpacity
+                style={styles.secondaryButton}
+                onPress={() => navigate("Signup")}>
+                <Text style={styles.secondaryButtonText}>Sign Up</Text>
+              </TouchableOpacity>
+            </View>
           </>
         )}
       </KeyboardAvoidingView>
     </View>
+    </ScrollView>
   );
 };
 
 export default Login;
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 20,
-    flex: 1,
+    flexGrow: 1,
+    backgroundColor: "#F9FAFB",
     justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 24,
+  },
+  scroll: {
+    backgroundColor: "#F9FAFB",
+    padding: 10,
+    width: "100%",
+  },
+  innerContainer: {
+    width: "100%",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "600",
+    marginBottom: 24,
+    color: "#111827",
+    textAlign: "center",
   },
   input: {
-    marginVertical: 4,
-    height: 50,
+    height: 52,
+    borderColor: "#D1D5DB",
     borderWidth: 1,
-    borderRadius: 4,
-    padding: 10,
-    backgroundColor: "#fff",
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    backgroundColor: "#FFFFFF",
+    marginBottom: 16,
+    fontSize: 16,
+    color: "#111827",
+  },
+  button: {
+    backgroundColor: "#007AFF",
+    paddingVertical: 16,
+    borderRadius: 10,
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  secondaryButton: {
+    alignItems: "center",
+  },
+  secondaryButtonText: {
+    color: "#007AFF",
+    fontWeight: "600",
+    fontSize: 16,
+    textDecorationLine: 'underline',
+
+  },
+  subtitleText: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#545f75",
+    textAlign: "center",
+    marginBottom:12,
+  },
+  googleButton: {
+    backgroundColor: "transparent",
+    borderWidth: 1,
+    borderColor: "#007AFF",
+    textAlign: "center",
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: "center",
+    paddingVertical: 16,
+    borderRadius: 10,
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  googleButtonText: {
+    textAlign: "center",
+    color: "#007AFF",
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  loader: {
+    marginTop: 20,
   },
 });
